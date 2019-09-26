9a2c14206e5caa426ff8ac5ebe812c2b8a7dafd6
Emil V. Toma <evtoma@gmail.com>
2019-09-26 06:41:20 +0100

    1.Folder "Models" added in root; 2.class model file "Movie.cs" added in "Models";

---
+++ Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DataType ReleaseDate { get; set; }
+        public string Genre { set; get; }
+        public decimal Price { set; get; }
+    }
+}
